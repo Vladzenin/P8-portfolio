af96f73dc61237dd088c7b0dab27a0099e2e47fa
julien ferté <julienfertenissan@gmail.com>
2024-10-09 18:06:29 +0200

    src spaceman

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import close from "./close.svg";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+
+import grimoire from "./grimoire.png";
+import carducci from "./carducci.png";
+import bluel from "./bluel.png";
+
+export { grimoire, close, carducci, logo, menu, bluel };
+
